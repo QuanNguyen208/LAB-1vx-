70e3f41eb6a1ec90ab9cca0c9e2feec2e445301c
QuanNguyen208 <quan.nguyen17@hcmut.edu.vn>
2022-12-24 22:09:31 +0700

    first commit

---
+++ Core/Inc/7seg_clock.h
@@ -0,0 +1,13 @@
+/*
+ * 7seg_clock.h
+ *
+ *  Created on: Nov 9, 2022
+ *      Author: Admin
+ */
+
+#ifndef INC_7SEG_CLOCK_H_
+#define INC_7SEG_CLOCK_H_
+
+void display7SEG(int idx , int num) ;
+
+#endif /* INC_7SEG_CLOCK_H_ */
